f0c94d8d29442e9bbca7deeb5e01dffb35ffa817
James McDonald <james@jamesmcdonald.com>
2017-10-12 14:05:51 +0200

    Refactor into a class and deal with most linting warnings

--- swarmer.py
+++ swarmer.py
@@ -1,109 +1,147 @@
-import docker
+#!/usr/bin/env python3
+"""Prometheus service discovery for Docker Swarm"""
 import json
 import logging
 from time import sleep
 from pprint import pformat
+import docker
 
-log = logging
+# Default settings
+DEFAULT_PATH = '/etc/prometheus/swarm.d/swarm-endpoints.json'
+DEFAULT_LABEL_NAME = 'prometheus.port'
+DEFAULT_ENV_NAME = 'SERVICE_PORTS'
+DEFAULT_SERVICE_NAME = 'prometheus'
+DEFAULT_PCNETWORKS = ['proxy']
+
+log = logging.getLogger(__name__) # pylint: disable=C0103
 logging.basicConfig(level=logging.DEBUG)
 
-log.info("In A.D. 2101 prometheus-swarmer was beginning.")
-client = docker.from_env()
+class PrometheusSwarmer(object):
+    """PrometheusSwarmer scans for swarm services and generates prometheus config."""
 
-# The path to output the configuration to
-outputpath = '/etc/prometheus/swarm.d/swarm-endpoints.json'
+    def __init__(self, # pylint: disable=R0913
+                 outputpath=DEFAULT_PATH,
+                 label_name=DEFAULT_LABEL_NAME,
+                 env_name=DEFAULT_ENV_NAME,
+                 service_name=DEFAULT_SERVICE_NAME,
+                 pcnetworks=None,
+                ):
+        """Create a PrometheusSwarmer
 
-# Label name for prometheus port config
-label_name = 'prometheus.port'
+        outputpath - The path to output the configuration to
+        label_name - Service label to identify metrics port
+        env_name - Environment variable to identify metrics port
+        service_name - The name of the prometheus service
+        pcnetworks - Override prometheus service detection with a static network list
+        """
 
-# Environment variable for prometheus port config
-# SERVICE_PORTS is compatible with dockercloud/haproxy
-env_name = 'SERVICE_PORTS'
+        self.outputpath = outputpath
+        self.label_name = label_name
+        self.env_name = env_name
+        self.service_name = service_name
 
-# Name of the prometheus service to look for
-service_name = 'prometheus'
+        self.client = docker.from_env()
 
-# Look for a network called 'proxy' by default
-default_pcnetworks = ['proxy']
+        # Find which networks the prometheus service is on
+        if pcnetworks is None:
+            try:
+                pcnetworks = []
+                pservice = self.client.services.get(service_name)
+                ptask = pservice.tasks()[0]
+                for netattachment in ptask['NetworksAttachments']:
+                    pcnetworks.append(netattachment['Network']['Spec']['Name'])
+            except docker.errors.NotFound:
+                pcnetworks = DEFAULT_PCNETWORKS
+        self.pcnetworks = pcnetworks
 
-# Find which networks the prometheus service is on
-try:
-    pcnetworks = []
-    ps = client.services.get(service_name)
-    pt = ps.tasks()[0]
-    for na in pt['NetworksAttachments']:
-        pcnetworks.append(na['Network']['Spec']['Name'])
-except docker.errors.NotFound:
-    pcnetworks = default_pcnetworks
+        self.endpoints = []
 
-while True:
-    log.debug("Start discovery")
-    endpoints = []
-    for service in client.services.list():
-        name = service.attrs['Spec']['Name']
+    def discover(self):
+        """Discover the services exposing metrics endpoints"""
+        self.endpoints = []
+        for service in self.client.services.list():
+            name = service.attrs['Spec']['Name']
 
-        # Don't monitor prometheus itself (it can do that)
-        if name == service_name:
-            continue
-
-        # Service labels
-        slabels = service.attrs['Spec']['Labels']
-
-        cs = service.attrs['Spec']['TaskTemplate']['ContainerSpec']
-
-        # Container labels
-        clabels = {}
-        if 'Labels' in cs:
-            clabels = cs['Labels']
-
-        envs = []
-        if 'Env' in cs:
-            envs = cs['Env']
-
-        port = None
-        if label_name in slabels:
-            port = slabels[label_name]
-        else:
-            ports = [x.split('=')[1] for x in envs if x.startswith(env_name + '=')]
-            if len(ports) == 1:
-                # Support a comma-separated list - take the first
-                port = ports[0].split(',')[0]
-        if port is None:
-            log.debug("Unable to find port for service '{}', skipping".format(name))
-            continue
-
-        for task in service.tasks():
-            # Skip tasks for containers that the service manager doesn't run anymore
-            if task['DesiredState'] != 'running':
+            # Don't monitor prometheus itself (it can do that)
+            if name == self.service_name:
                 continue
 
-            # Add an endpoint for the first matching network
-            if 'NetworksAttachments' not in task:
-                log.debug("No network attachments for service '{}', skipping".format(name))
+            # Service labels
+            slabels = service.attrs['Spec']['Labels']
+
+            # Container labels
+            cspec = service.attrs['Spec']['TaskTemplate']['ContainerSpec']
+            clabels = cspec['Labels'] if 'Labels' in cspec else {}
+
+            # Environment variables
+            envs = cspec['Env'] if 'Env' in cspec else []
+
+            port = None
+            if self.label_name in slabels:
+                port = slabels[self.label_name]
+            else:
+                ports = [x.split('=')[1] for x in envs if x.startswith(self.env_name + '=')]
+                if len(ports) == 1:
+                    # Support a comma-separated list - take the first
+                    port = ports[0].split(',')[0]
+            if port is None:
+                log.debug("Unable to find port for service '%s', skipping", name)
                 continue
-            for network in task['NetworksAttachments']:
-                if network['Network']['Spec']['Name'] in pcnetworks:
-                    address = network['Addresses'][0].split('/')[0]
-                    endpoint = {
-                        'targets': [address + ':' + port],
-                        'labels': {
-                            'job': name
+
+            for task in service.tasks():
+                # Skip tasks for containers that the service manager doesn't run anymore
+                if task['DesiredState'] != 'running':
+                    continue
+
+                # Add an endpoint for the first matching network
+                if 'NetworksAttachments' not in task:
+                    log.debug("No network attachments for service '%s', skipping", name)
+                    continue
+                for network in task['NetworksAttachments']:
+                    if network['Network']['Spec']['Name'] in self.pcnetworks:
+                        address = network['Addresses'][0].split('/')[0]
+                        endpoint = {
+                            'targets': [address + ':' + port],
+                            'labels': {
+                                'job': name
+                            }
                         }
-                    }
-                    for label in slabels:
-                        l = label.replace('.', '_')
-                        endpoint['labels']['service_label_{}'.format(l)] = slabels[label]
-                    for label in clabels:
-                        l = label.replace('.', '_')
-                        endpoint['labels']['container_label_{}'.format(l)] = clabels[label]
-                    endpoint['labels']['container_id'] = \
-                        task['Status']['ContainerStatus']['ContainerID']
-                    endpoints.append(endpoint)
-                    log.debug('Add endpoint for {} at {}:{}'.format(name, address, port))
-                    break
+                        for label in slabels:
+                            legallabel = label.replace('.', '_')
+                            endpoint['labels']['service_label_{}'.format(legallabel)] = \
+                                    slabels[label]
+                        for label in clabels:
+                            legallabel = label.replace('.', '_')
+                            endpoint['labels']['container_label_{}'.format(legallabel)] \
+                                    = clabels[label]
+                        endpoint['labels']['container_id'] = \
+                            task['Status']['ContainerStatus']['ContainerID']
+                        self.endpoints.append(endpoint)
+                        log.debug('Add endpoint for %s at %s:%s', name, address, port)
+                        break
 
-    log.debug(pformat(endpoints))
-    with open(outputpath, "w") as jsonfile:
-        json.dump(endpoints, jsonfile)
-    log.debug("Finish discovery")
-    sleep(60)
+        log.debug(pformat(self.endpoints))
+
+
+    def writejson(self):
+        """Write the discovered endpoints to the config file"""
+        with open(self.outputpath, "w") as jsonfile:
+            json.dump(self.endpoints, jsonfile)
+
+    def run(self):
+        """Run discovery in an infinite loop"""
+        log.info("In A.D. 2101 prometheus-swarmer was beginning.")
+        while True:
+            log.debug("Start discovery")
+            self.discover()
+            log.debug("Finish discovery")
+            self.writejson()
+            sleep(60)
+
+def main():
+    """Main function for running class directly"""
+    swarmer = PrometheusSwarmer()
+    swarmer.run()
+
+if __name__ == '__main__':
+    main()
